018c3e6fb9dac088c1014ee97d9637cd16ce1d1f
muhsaputra <muhsaputrabiz@gmail.com>
2022-06-18 17:14:39 +0700

    portfolio lostvayne

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: ['Poppins'],
+      },
+      colors: {
+        primary: '#14b8a6'
+      }
+    },
+  },
+  plugins: [],
+}
